44a92b8ac3b010919a73ea9091fa32d893146360
Fahd <fahdabdullai5@gmail.com>
2023-10-09 12:28:54 +0100

    Sage

---
+++ florex.js
@@ -0,0 +1,11 @@
+  alert("Do you want to visit Nak-Florex website?");
+
+
+// function msg() {
+//   var m =confirm("Are you sure?");
+//   if (true) {
+//     alert("Ok");
+//   else {
+//     alert("Cancel");
+//   }
+//   }
